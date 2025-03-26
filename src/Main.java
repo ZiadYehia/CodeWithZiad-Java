090b643d53fadd1fbc11521758982d9e888e1647
‪ziad mohamed‬‏ yehia <ziadmohamedyehia123@gmail.com>
2025-03-27 00:38:51 +0200

    Task2

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        String studentName = "Ziad Mohamed Yehia";
+        int graduationYear = 2023;
+        char grade = 'A';
+
+        System.out.println("Student Name is "+ studentName +"\nGraduation Year :"+ graduationYear +"\nGrade :"+ grade);
+    }
+
+}
